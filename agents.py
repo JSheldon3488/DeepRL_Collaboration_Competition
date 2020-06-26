bfcbd1a351317aa20c8b82a0bd48ed5633b5544e
Justin Sheldon <JSheldon3488@gmail.com>
2020-06-25 23:54:03 -0500

    Passed Agent

--- agents.py
+++ agents.py
@@ -1,55 +1,48 @@
 from networks import Actor, Critic
 from utils import OUNoise, ReplayBuffer
 
-import time
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
-import os
-import random
 import numpy as np
 from collections import deque
+import os
 PATH = os.path.abspath(os.getcwd())
 
 class MADDPG():
-    """ Multi Agent Deep Deterministic Policy Gradients Agent used to interaction with and learn from an environment """
+    """ Multi Agent Deep Deterministic Policy Gradients Agent used to interaction with and learn from an environment.
+        This current iteration is not actually MADDPG. It is two individual DDPG agents with a wrapper class MADDPG
+        that handles using two seperate agents. (Could not get true MADDPG to work)
+     """
 
     def __init__(self, state_size: int, action_size: int, num_agents: int, epsilon: float, seed: int):
         """ Initialize a MADDPG Agent Object
-        :param state_size: dimension of state (input)
-        :param action_size: dimension of action (output)
+        :param state_size: dimension of state per agent (input)
+        :param action_size: dimension of action per agent (output)
         :param num_agents: number of concurrent agents in the environment
         :param epsilon: initial value of epsilon for exploration
         :param random_seed: random seed
         """
-        # TODO: What parameters do we not need for MADDPG
         self.state_size = state_size
         self.action_size = action_size
         self.num_agents = num_agents
         self.seed = seed
-        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.t_step = 0
 
         # Hyperparameters
-        self.buffer_size = 100000
-        self.batch_size = 256
         self.update_every = 1
         self.num_updates = 1
-        self.gamma = 0.99
-        self.epsilon = 1
-        self.epsilon_decay = 0 # Turned off noise scaling
-        self.epsilon_min = 1
+        self.epsilon = epsilon
+        self.epsilon_decay = .998
+        self.epsilon_min = 0
 
         # Setup up all Agents
-        self.agents = [DDPG(self.state_size, self.action_size, self.num_agents, self.seed) for _ in range(self.num_agents)]
-
-        # Replay Buffer Setup
-        self.memory = ReplayBuffer(self.buffer_size, self.batch_size)
+        self.agents = [DDPG(self.state_size, self.action_size, self.seed) for _ in range(self.num_agents)]
 
     def __str__(self):
         return "MADDPG_Agent"
 
-    def train(self, env, brain_name, num_episodes=2500, print_every=10):
+    def train(self, env, brain_name, num_episodes=1000, print_every=25):
         """ Interacts with and learns from a given Unity Environment
         :param env: Unity Environment the agents is trying to learn
         :param brain_name: Brain for Environment
@@ -67,26 +60,14 @@ class MADDPG():
             # Reset everything
             episode_scores = np.zeros(self.num_agents)
             env_info = env.reset(train_mode=True)[brain_name]
-            states = env_info.vector_observations
-            # print(f'Original Shape of States: Expected = [2,24], Actual = {states.shape}') PASSED
+            states = env_info.vector_observations #[2,24]
             self.reset_noise()
-            # print(f'Test of noise sample: Expected = [2,2] with numbers from standard normal, Actual = {[agent.noise.sample() for agent in self.agents]}') PASSED
 
             # Run the episode
             while True:
                 actions = self.act(states, self.epsilon)
-                # print(f'Actions Expected Shape: [2,2] Actual Shape: {actions.shape}') PASSED
                 env_info = env.step(actions)[brain_name]
                 next_states, rewards, dones = env_info.vector_observations, env_info.rewards, env_info.local_done
-
-                '''print("--------- Checking Replay Buffer Input ------------")
-                print(f'States: {states.shape}')
-                print(f'Actions: {actions.shape}')
-                print(f'Rewards: {rewards}')                                    PASSED
-                print(f'Next_States: {next_states.shape}')
-                print(f'Dones: {dones}')
-                print("----------------------------------------------------") '''
-
                 self.step(states, actions, rewards, next_states, dones)
                 episode_scores += rewards
                 states = next_states
@@ -94,7 +75,7 @@ class MADDPG():
                     break
 
             # -------- Episode Finished ---------#
-            self.epsilon -= self.epsilon_decay
+            self.epsilon *= self.epsilon_decay
             self.epsilon = max(self.epsilon, self.epsilon_min)
             scores.append(np.max(episode_scores))
             scores_deque.append(np.max(episode_scores))
@@ -124,121 +105,40 @@ class MADDPG():
         return scores, avg_scores
 
     def step(self, states, actions, rewards, next_states, dones):
-        """ what the agent needs to do for every time step that occurs in the environment. Takes
-        in a (s,a,r,s',d) tuple and saves it to memory and learns from experiences. Note: this is not
-        the same as a step in the environment. Step is only called once per environment time step.
+        """ parse and store the experience for each individual agent and call individual agent step every update_every timesteps
+
         :param states: array of states agents used to select actions
         :param actions: array of actions taken by agents
         :param rewards: array of rewards from the environment for the last actions taken by the agents
         :param next_states: array of next states after actions were taken by the agents
         :param dones: array of bools representing if environment is finished or not
         """
-        self.memory.add(states, actions, rewards, next_states, dones) # Note these contain info for both agents
-
-        # Learn "num_updates" times every "update_every" time step
         self.t_step += 1
-        if len(self.memory) > self.batch_size and self.t_step%self.update_every == 0:
+        for i,agent in enumerate(self.agents):
+            agent.memory.add(states[i], actions[i], rewards[i], next_states[i], dones[i])
+        if self.t_step%self.update_every == 0:
             self.t_step = 0
-            for agent_num in range(self.num_updates):
-                self.learn(self.memory.sample(), agent_num)
+            for agent in self.agents:
+                agent.step()
 
-    def act(self, states, epsilon, add_noise=True):
-        """ Returns actions for each agent given states as per current policy. Policy comes from the actor network.
+    def act(self, states, epsilon):
+        """ Returns actions for each agent given states as per current policy. Policy comes from the agents actor network.
         :param states: array of states from the environment for each agent
         :param epsilon: probability of exploration
-        :param add_noise: bool on whether or not to potentially have exploration for action
         :return: clipped actions for all agents from the local networks
         """
         actions = [agent.act(local_obs, epsilon) for agent, local_obs in zip(self.agents,states)]
         return np.asarray(actions)
 
-    def target_act(self, obs_full):
-        """ Returns actions for each agent given states as per current policy. Policy comes from the actor network.
-        :param states: array of states from the environment for each agent
-        :return: clipped actions for all agents from the local networks
-        """
-        actions = [agent.target_act(local_obs) for agent, local_obs in zip(self.agents,obs_full)]
-        return np.asarray(actions)
-
     def reset_noise(self):
-        """ resets to noise parameters for all actors """
+        """ resets to noise parameters for all agents """
         for agent in self.agents:
             agent.reset_noise()
 
-    def learn(self, experiences, agent_num):
-        """ Update actor and critic networks for agent using a given batch of experiences """
-
-        full_states, full_actions, rewards, full_next_states, dones = experiences
-
-        '''
-        print('---------------Check Experience Output -------------------')
-        print(f'full_states type: {type(full_states)}, shape: {full_states.shape}') # [256,2,24]
-        print(f'full_actions type: {type(full_actions)}, shape: {full_actions.shape}') # [256,2,2]
-        print(f'rewards type: {type(rewards)}, shape: {rewards.shape}') # [256,2]                   PASSED
-        print(f'full_next_states type: {type(full_next_states)}, shape: {full_next_states.shape}') # [256,2,24]
-        print(f'dones type: {type(dones)}, shape: {dones.shape}') # [256,2]
-        print("")
-        '''
-
-        # -------------------- Update Critic --------------------
-        ''' We need the target critic values for each agent where the input to the critic network for that agent is full next state information (both agents info)
-            and each agents target action selection that would be selected based on that info. So for a single example from the full_states mini batch
-            you need to get the actions for both agents based on their target actor network and their chunk of the observation. Then you combine
-            the full observation and the actinos and feed them through the target critic for that agent (will happen for both), multiply it by the discount rate
-            and add it to the reward to get y = two different values one for each agent.
-        '''
-        # Get next actions for all examples in the mini batch
-        next_actions = torch.from_numpy(np.asarray([self.target_act(full_states[batch_num]) for batch_num in range(self.memory.batch_size)])).float().to(self.device)
-        # print(f'Next Action type: {type(next_actions)}, Shape: {next_actions.shape}') #[256,2,2] PASSED
-
-        # Get Q_values from target network for next_state and next_actions (Need to change next_states into [256,48] and next_actions into [256,4] for each agent
-        full_next_states = full_next_states.view(self.memory.batch_size,-1)
-        next_actions = next_actions.view(self.memory.batch_size,-1)
-        # print(f'Full_Next_States type: {type(full_next_states)}, shape: {full_next_states.shape}') PASSED
-        # print(f'Next_Actions type: {type(next_actions)}, shape: {next_actions.shape}') PASSED
-        next_q_targets = []
-        for agent in self.agents:
-            next_q_targets.append(agent.critic_target(full_next_states, next_actions))
-        # print(f'Next_q_targets type: {type(next_q_targets)}, shape: {next_q_targets[0].shape}') # List of Tensors that are [256,1]
-
-        # Calclate y which is the q_targets = rewards for agent i + discount*q_targets for agent i (calculated for both agents)
-        q_targets = [rewards[:,agent_num] + ((self.gamma*next_q_targets[agent_num]).view(-1))*(1-dones[:,agent_num]) for agent_num in range(self.num_agents)]
-        # print(f'q_targets type: {type(q_targets[0])}, shape: {len(q_targets)},{q_targets[0].shape}') # 2 tensors of size [256] PASSED
-        # Get Q_expected
-        q_expected = []
-        for agent in self.agents:
-            q_expected.append(agent.critic_local(full_states.view(self.memory.batch_size,-1), full_actions.view(self.memory.batch_size,-1)).view(-1))
-        # print(f'Q_Expected type: {type(q_expected[0])}, shape: {len(q_expected)},{q_expected[0].shape} ') 2 tensors of size [256] PASSED
-
-        # Compute critic loss
-        critic_loss = []
-        for i, agent in enumerate(self.agents):
-            critic_loss.append(F.mse_loss(q_targets[i],q_expected[i]))
-            agent.critic_optimizer.zero_grad()
-            critic_loss[i].backward()
-            agent.critic_optimizer.step()
-
-
-        # -------------------- Update Actor --------------------- #
-        # Computer actor loss (maximize mean of Q(states,actions))
-        # Optimizer minimizes and we want to maximize so multiply by -1
-        for i,agent in enumerate(self.agents):
-            actor_loss = -1*agent.critic_local(full_states.view(self.memory.batch_size,-1), full_actions.view(self.memory.batch_size,-1)).mean()
-            # Minimize the negative loss (maximize q_value)
-            agent.actor_optimizer.zero_grad()
-            actor_loss.backward()
-            agent.actor_optimizer.step()
-
-        # TODO: Make sure soft updates correct
-        #---------------- Update Target Networks ---------------- #
-        for agent in self.agents:
-            agent.soft_update(agent.actor_local, agent.actor_target, agent.tau)
-
-
 class DDPG():
-    """ This is a decentralized actor used by the MADDPG Agent """
+    """ This is an Individual DDPG Agent """
 
-    def __init__(self, state_size, action_size, num_agents, seed):
+    def __init__(self, state_size, action_size, seed):
         """ Initialize a DDPG Agent Object
         :param state_size: dimension of state (input) for this decentralized actor
         :param action_size: dimension of action (output) for this decentralized actor
@@ -246,31 +146,34 @@ class DDPG():
         """
         self.state_size = state_size
         self.action_size = action_size
-        self.num_agents = num_agents
         self.seed = seed
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
         # Hyperparameters
-        self.tau = 0.05
-        self.lr_actor = 0.001
+        self.buffer_size = 100000
+        self.batch_size = 256
+        self.gamma = 0.99
+        self.tau = 0.01
+        self.lr_actor = 0.0001
         self.lr_critic = 0.001
 
         # Setup Networks (Actor: State -> Action, Critic: (States for all agents, Actions for all agents) -> Value)
         self.actor_local = Actor(self.state_size, self.action_size,  self.seed).to(self.device)
         self.actor_target = Actor(self.state_size, self.action_size, self.seed).to(self.device)
         self.actor_optimizer = optim.Adam(self.actor_local.parameters(), lr = self.lr_actor)
-
-        self.critic_local = Critic(self.state_size*self.num_agents, self.action_size*self.num_agents, self.seed).to(self.device)
-        self.critic_target = Critic(self.state_size*self.num_agents, self.action_size*self.num_agents, self.seed).to(self.device)
+        self.critic_local = Critic(self.state_size, self.action_size, self.seed).to(self.device)
+        self.critic_target = Critic(self.state_size, self.action_size, self.seed).to(self.device)
         self.critic_optimizer = optim.Adam(self.critic_local.parameters(), lr = self.lr_critic)
 
         # Initialize local and taret networks to start with same parameters
         self.soft_update(self.actor_local, self.actor_target, tau=1)
         self.soft_update(self.critic_local, self.critic_target, tau=1)
 
-        # TODO: Make sure Noise is returning correct size and values to add noise to actions
         # Noise Setup
         self.noise = OUNoise(self.action_size, self.seed)
+
+        # Replay Buffer Setup
+        self.memory = ReplayBuffer(self.buffer_size, self.batch_size)
 
     def __str__(self):
         return "DDPG_Agent"
@@ -291,24 +194,51 @@ class DDPG():
         with torch.no_grad():
             actions = self.actor_local(state).cpu().data.numpy()
         self.actor_local.train()
-        # print(f'Action before noise: Shape: {actions.shape}, Value: {actions}') PASSED
-        if add_noise:
-            actions += self.noise.sample()*epsilon
-        # print(f'Action after noise: Shape: {actions.shape}, Value: {actions}') PASSED
+        if add_noise and epsilon > np.random.random():
+            actions += self.noise.sample()
         return np.clip(actions, -1,1)
 
-    def target_act(self, state):
-        """ Used for selectiong actions for next_states. No exploration needed
+    def step(self):
+        if len(self.memory) > self.batch_size:
+            experiences = self.memory.sample()
+            self.learn(experiences)
 
-        :param state: observations for this indiviual actoin (next_state)
-        :return: numpy array of clipped actions for this agent
+    def learn(self, experiences):
+        """ Update actor and critic networks using a given batch of experiences
+        Q_targets = r + γ * critic_target(next_state, actor_target(next_state))
+        where:
+            actor_target(states) -> actions
+            critic_target(states, actions) -> Q-value
+        :param experiences: tuple of arrays (states, actions, rewards, next_states, dones)  sampled from the replay buffer
         """
-        self.actor_target.eval()
-        with torch.no_grad():
-            action = self.actor_target(state).cpu().data.numpy()
-        self.actor_target.train()
-        return np.clip(action, -1, 1)
 
+        states, actions, rewards, next_states, dones = experiences
+        # -------------------- Update Critic -------------------- #
+        # Use target networks for getting next actions and q values and calculate q_targets
+        next_actions = self.actor_target(next_states)
+        next_q_targets = self.critic_target(next_states, next_actions)
+        q_targets = rewards + (self.gamma * next_q_targets * (1 - dones))
+        # Compute critic loss (Same as DQN Loss)
+        q_expected = self.critic_local(states, actions)
+        critic_loss = F.mse_loss(q_expected, q_targets)
+        # Minimize loss
+        self.critic_optimizer.zero_grad()
+        critic_loss.backward()
+        self.critic_optimizer.step()
+
+        # -------------------- Update Actor --------------------- #
+        # Computer actor loss (maximize mean of Q(states,actions))
+        action_preds = self.actor_local(states)
+        # Optimizer minimizes and we want to maximize so multiply by -1
+        actor_loss = -1 * self.critic_local(states, action_preds).mean()
+        # Minimize the loss
+        self.actor_optimizer.zero_grad()
+        actor_loss.backward()
+        self.actor_optimizer.step()
+
+        # ---------------- Update Target Networks ---------------- #
+        self.soft_update(self.critic_local, self.critic_target, self.tau)
+        self.soft_update(self.actor_local, self.actor_target, self.tau)
 
     def soft_update(self, local_network, target_network, tau):
         """ soft update newtwork parametes
--- networks.py
+++ networks.py
@@ -13,7 +13,6 @@ def hidden_init(layer):
 
 class Actor(nn.Module):
     """ Actor (Policy) network for action selection """
-    # TODO: Make sure action input state_size is correctly handled (should it be 24 or 48)
     def __init__(self, input_size, action_size, seed, fc1_size=512, fc2_size=256, leak=0.01):
         """ Initialize parameters and build actor network
         :param input_size: Dimension of the input
@@ -21,7 +20,6 @@ class Actor(nn.Module):
         :param seed: random seed
         """
         super(Actor,self).__init__()
-        # TODO: Figure out what was going worng with my seed
         self.seed = torch.manual_seed(seed)
         self.leak = leak
         self.bn_input = nn.BatchNorm1d(input_size)
@@ -33,15 +31,16 @@ class Actor(nn.Module):
 
     def forward(self, states):
         """ policy network that maps states -> actions """
-        ''' Noralization and leaky relu
+        ''' 
+        Forward pass with Normalization and leaky relu activations
         x = self.bn_input(states)
         x = F.leaky_relu(self.fc1(states), negative_slope=self.leak)
         x = F.leaky_relu(self.fc2(x), negative_slope=self.leak)
+        return torch.tanh(self.fc3(x))
         '''
-        # No Normalization or leaky relu
+        # Forward pass with no Normalization or leaky relu
         x = F.relu(self.fc1(states))
         x = F.relu(self.fc2(x))
-
         return torch.tanh(self.fc3(x))
 
     def reset_parameters(self):
@@ -72,13 +71,15 @@ class Critic(nn.Module):
 
     def forward(self, states, actions):
         """ Critic Network that maps (states,actions) pairs -> Q-Values """
-        ''' With Normalization and leaky relu
+        ''' 
+        Forward pass with Normalization and leaky relu activations
         x = self.bn_input(states)
         x = F.leaky_relu(self.bn1(self.fc1(states)), negative_slope=self.leak)
         x_a = torch.cat((x,actions), dim=1)
         x_a = F.leaky_relu(self.fc2(x_a), negative_slope=self.leak)
+        return self.fc3(x_a)
         '''
-        # Without Normalization and Leaky Relu
+        # Forward Pass Without Normalization and Leaky Relu
         x = F.relu(self.fc1(states))
         x_a =  torch.cat((x, actions), dim=1)
         x_a = F.relu(self.fc2(x_a))
